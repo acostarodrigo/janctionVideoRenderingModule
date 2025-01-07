8b14f5eee7b5d5493f9e159e0f7723661807aec8
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2025-01-07 14:17:48 -0400

    added new worker error

--- errors.go
+++ errors.go
@@ -5,4 +5,6 @@ import "cosmossdk.io/errors"
 var (
 	ErrIndexTooLong     = errors.Register(ModuleName, 2, "index too long")
 	ErrDuplicateAddress = errors.Register(ModuleName, 3, "duplicate address")
+
+	ErrWorkerAlreadyRegistered = errors.Register(ModuleName, 4, "worker already registered")
 )
