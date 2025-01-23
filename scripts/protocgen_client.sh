c0c5ab4b25e4c128aea6317b434870d9e89396ea
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2025-01-23 18:18:40 -0400

    added client for TS

--- scripts/protocgen_client.sh
+++ scripts/protocgen_client.sh
@@ -6,7 +6,14 @@ cd proto
 proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-  echo $file
+    echo $file
+
+    # Skip google/protobuf files to avoid conflicts
+    if [[ $file == *"google/protobuf/"* ]]; then
+      echo "Skipping $file to avoid conflicts with protobufjs"
+      continue
+    fi
+
     # this regex checks if a proto file has its go_package set to github.com/janction/videoRendering/api/...
     # gogo proto files SHOULD ONLY be generated if this is false
     # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
