a1b57378f420571440abe6818a95845ac70022b0
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2025-02-05 14:26:43 -0400

    new method to get public ip

--- Worker.go
+++ Worker.go
@@ -1,8 +1,11 @@
 package videoRendering
 
 import (
+	io "io"
 	"log"
+	"net/http"
 	"os/exec"
+	"strings"
 
 	"github.com/janction/videoRendering/db"
 )
@@ -10,7 +13,8 @@ import (
 func (w Worker) RegisterWorker(address string, db *db.DB) error {
 	db.Addworker(address)
 	executableName := "janctiond"
-	cmd := exec.Command(executableName, "tx", "videoRendering", "add-worker", "--from", address, "--yes")
+	ip, _ := getPublicIP()
+	cmd := exec.Command(executableName, "tx", "videoRendering", "add-worker", ip, "--from", address, "--yes")
 	_, err := cmd.Output()
 	log.Printf("executing %s", cmd.String())
 	if err != nil {
@@ -18,4 +22,20 @@ func (w Worker) RegisterWorker(address string, db *db.DB) error {
 		return err
 	}
 	return nil
+}
+
+// GetPublicIP fetches the public IP of the machine
+func getPublicIP() (string, error) {
+	resp, err := http.Get("https://api.ipify.org")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	ip, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(ip)), nil
 }
