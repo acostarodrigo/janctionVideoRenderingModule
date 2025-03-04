f096368309e29dd61b0a15e90afc3dbca35446ec
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2025-03-04 17:04:54 -0400

    new utils functions

---
+++ utils.go
@@ -0,0 +1,54 @@
+package videoRendering
+
+import (
+	fmt "fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+// Transforms a slice with format [key]=[value] to a map
+func TransformSliceToMap(input []string) (map[string]string, error) {
+	result := make(map[string]string)
+
+	for _, item := range input {
+		parts := strings.SplitN(item, "=", 2) // Split into 2 parts: filename and hash
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid format: %s", item)
+		}
+		filename := parts[0]
+		hash := parts[1]
+		result[filename] = hash
+	}
+
+	return result, nil
+}
+
+// MapToKeyValueFormat converts a map[string]string to a "key=value,key=value" format
+func MapToKeyValueFormat(inputMap map[string]string) []string {
+	var parts []string
+
+	// Iterate through the map and build the key=value pairs
+	for key, value := range inputMap {
+		parts = append(parts, fmt.Sprintf("%s=%s", key, value))
+	}
+
+	// Join the key=value pairs with commas
+	return parts
+}
+
+// Executes a cli command with their arguments
+func ExecuteCli(args []string) error {
+	executableName := "janctiond"
+	cmd := exec.Command(executableName, args...)
+	log.Printf("Executing %s", cmd.String())
+
+	_, err := cmd.Output()
+
+	if err != nil {
+		log.Printf("Error Executing %s", err.Error())
+		return err
+	}
+
+	return nil
+}
