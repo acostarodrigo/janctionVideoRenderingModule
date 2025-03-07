4e91edad7c48cfc6e46dd3b5f831dbc6836f0bb9
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2025-03-07 16:51:39 -0400

    added levels and default level

--- videoRenderingLogger/videoRenderingLogger.go
+++ videoRenderingLogger/videoRenderingLogger.go
@@ -14,32 +14,47 @@ const (
 	colorBlue  = "\033[34m"
 )
 
+// Log levels
+const (
+	LevelInfo  = 1
+	LevelDebug = 2
+	LevelError = 3
+)
+
 // VideoRenderingLogger defines a custom logger for the module
 type VideoRenderingLogger struct {
-	logger *log.Logger
+	logger   *log.Logger
+	logLevel int
 }
 
-// NewVideoRenderingLogger creates a new instance of the logger
-func NewVideoRenderingLogger() *VideoRenderingLogger {
+// NewVideoRenderingLogger creates a new instance of the logger with a specified log level
+func NewVideoRenderingLogger(level int) *VideoRenderingLogger {
 	return &VideoRenderingLogger{
-		logger: log.New(os.Stdout, colorRed+"[VideoRendering] ", log.LstdFlags),
+		logger:   log.New(os.Stdout, colorRed+"[VideoRendering] ", log.LstdFlags),
+		logLevel: level,
 	}
 }
 
-// GlobalLogger provides a globally accessible logger instance
-var Logger = NewVideoRenderingLogger()
+// GlobalLogger provides a globally accessible logger instance with default level INFO
+var Logger = NewVideoRenderingLogger(LevelInfo)
 
-// Info logs informational messages (Green)
+// Info logs informational messages (Green) if log level allows
 func (v *VideoRenderingLogger) Info(format string, args ...interface{}) {
-	v.logger.Println(colorGreen + "INFO: " + fmt.Sprintf(format, args...) + colorReset)
+	if v.logLevel <= LevelInfo {
+		v.logger.Println(colorGreen + "INFO: " + fmt.Sprintf(format, args...) + colorReset)
+	}
 }
 
-// Error logs error messages (Red)
+// Error logs error messages (Red) if log level allows
 func (v *VideoRenderingLogger) Error(format string, args ...interface{}) {
-	v.logger.Println(colorRed + "ERROR: " + fmt.Sprintf(format, args...) + colorReset)
+	if v.logLevel <= LevelError {
+		v.logger.Println(colorRed + "ERROR: " + fmt.Sprintf(format, args...) + colorReset)
+	}
 }
 
-// Debug logs debug messages (Blue)
+// Debug logs debug messages (Blue) if log level allows
 func (v *VideoRenderingLogger) Debug(format string, args ...interface{}) {
-	v.logger.Println(colorBlue + "DEBUG: " + fmt.Sprintf(format, args...) + colorReset)
+	if v.logLevel <= LevelDebug {
+		v.logger.Println(colorBlue + "DEBUG: " + fmt.Sprintf(format, args...) + colorReset)
+	}
 }
