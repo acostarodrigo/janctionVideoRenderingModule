2ee8a66074b21fab16699ac6cfd9e4567d94313a
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2025-03-11 13:39:59 -0400

    new package and test to handle crypto

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -1,18 +1,61 @@
 package videoRenderingCrypto_test
 
 import (
-	"fmt"
 	"testing"
 
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	videoRenderingCrypto "github.com/janction/videoRendering/crypto"
 )
 
-func TestWorkerSign(t *testing.T) {
+func TestPublicKeyMatch(t *testing.T) {
+	cdc := moduletestutil.MakeTestEncodingConfig().Codec
+	message := []byte("Validate file possession") // The message to sign
+	pk, err := videoRenderingCrypto.GetPublicKey("/Users/rodrigoacosta/.janctiond", "alice", cdc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, publicKey, err := videoRenderingCrypto.SignMessage("/Users/rodrigoacosta/.janctiond", "alice", message, cdc)
+	if err != nil {
+		t.Error(err)
+	}
+	if pk.String() != publicKey.String() {
+		t.Errorf("invalid public key %s != %s", pk.String(), publicKey.String())
+	}
+}
+func TestWorkerSignAndValidation(t *testing.T) {
 	message := []byte("Validate file possession") // The message to sign
 	cdc := moduletestutil.MakeTestEncodingConfig().Codec
 	signature, publicKey, err := videoRenderingCrypto.SignMessage("/Users/rodrigoacosta/.janctiond", "alice", message, cdc)
-	fmt.Println("error", err)
-	fmt.Println("signature", signature)
-	fmt.Println("publicKey", publicKey)
+	if err != nil {
+		t.Error(err)
+	}
+
+	valid := publicKey.VerifySignature(message, signature)
+	if !valid {
+		t.Error("Signature is not valid")
+	} else {
+		t.Log("Signature is valid")
+	}
+}
+
+func TestSerialization(t *testing.T) {
+	cdc := moduletestutil.MakeTestEncodingConfig().Codec
+	pk, err := videoRenderingCrypto.GetPublicKey("/Users/rodrigoacosta/.janctiond", "alice", cdc)
+	if err != nil {
+		t.Error(err)
+	}
+	protoPk, err := videoRenderingCrypto.PubKeyToProto(pk)
+	if err != nil {
+		t.Error(err)
+	}
+
+	serializedPk, err := videoRenderingCrypto.ProtoToPubKey(*protoPk)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if pk.String() != serializedPk.String() {
+		t.Errorf("incorrect serialization")
+	}
 }
--- crypto/videoRenderingCrypto.go
+++ crypto/videoRenderingCrypto.go
@@ -1,10 +1,11 @@
 package videoRenderingCrypto
 
 import (
-	"crypto/sha256"
 	"fmt"
 
+	protocrypto "github.com/cometbft/cometbft/proto/tendermint/crypto"
 	"github.com/cosmos/cosmos-sdk/codec"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
@@ -30,10 +31,26 @@ func getKeyRing(rootDir string, codec codec.Codec) (keyring.Keyring, error) {
 	if len(keys) == 0 {
 		videoRenderingLogger.Logger.Info("No keys found in keyring")
 	} else {
-		videoRenderingLogger.Logger.Info("Loaded keys:", keys)
+		videoRenderingLogger.Logger.Info("Loaded %v keys succesfully", len(keys))
 	}
 
 	return kr, nil
+}
+
+func GetPublicKey(rootDir, alias string, codec codec.Codec) (cryptotypes.PubKey, error) {
+	keyRing, err := getKeyRing(rootDir, codec)
+	if err != nil {
+		videoRenderingLogger.Logger.Error("Unable to load key ring at %s: %s", rootDir, err.Error())
+		return nil, err
+	}
+
+	k, err := keyRing.Key(alias)
+	if err != nil {
+		videoRenderingLogger.Logger.Error("unable to load key for %s: %s", alias, err.Error())
+		return nil, err
+	}
+	pk, _ := k.GetPubKey()
+	return pk, nil
 }
 
 func SignMessage(rootDir, alias string, message []byte, codec codec.Codec) ([]byte, cryptotypes.PubKey, error) {
@@ -42,24 +59,15 @@ func SignMessage(rootDir, alias string, message []byte, codec codec.Codec) ([]by
 		videoRenderingLogger.Logger.Error("Unable to load key ring at %s: %s", rootDir, err.Error())
 		return nil, nil, err
 	}
-	keys, err := keyRing.List()
-	if err != nil {
-		videoRenderingLogger.Logger.Error("Error listing keys: %s", err.Error())
-	} else {
-		if len(keys) == 0 {
-			videoRenderingLogger.Logger.Error("keyring doesn't contain any keys")
-			return nil, nil, fmt.Errorf("keyring doesn't contain any keys")
-		}
-	}
 
 	_, err = keyRing.Key(alias)
+
 	if err != nil {
 		videoRenderingLogger.Logger.Error("Key %s not found in keyring: %s", alias, err.Error())
 		return nil, nil, err
 	}
 
-	hash := sha256.Sum256(message)
-	signature, pubKey, err := keyRing.Sign(alias, hash[:], signing.SignMode_SIGN_MODE_DIRECT)
+	signature, pubKey, err := keyRing.Sign(alias, message, signing.SignMode_SIGN_MODE_DIRECT)
 	if err != nil {
 		videoRenderingLogger.Logger.Error("Error signing message: %s", err.Error())
 		return nil, nil, err
@@ -71,4 +79,43 @@ func SignMessage(rootDir, alias string, message []byte, codec codec.Codec) ([]by
 // checks if the signed message, correspond to the publick key
 func VerifyMessage(pubKey cryptotypes.PubKey, message []byte, signature []byte) bool {
 	return pubKey.VerifySignature(message, signature)
+}
+
+// extract public key for the specified alias from the Key ring
+func ExtractPublicKey(rootDir, alias string, codec codec.Codec) (cryptotypes.PubKey, error) {
+	kr, err := getKeyRing(rootDir, codec)
+	if err != nil {
+		return nil, err
+	}
+
+	// / Find the key
+	keyInfo, err := kr.Key(alias)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find key %s: %w", alias, err)
+	}
+
+	// Extract public key
+	pubKey, err := keyInfo.GetPubKey()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get public key: %w", err)
+	}
+	return pubKey, nil
+}
+
+func ProtoToPubKey(protoPubKey protocrypto.PublicKey) (cryptotypes.PubKey, error) {
+	// Correct way to unmarshal
+	pubKey, err := cryptocodec.FromCmtProtoPublicKey(protoPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal public key: %w", err)
+	}
+	return pubKey, nil
+}
+
+func PubKeyToProto(pk cryptotypes.PubKey) (*protocrypto.PublicKey, error) {
+	pubKey, err := cryptocodec.ToCmtProtoPublicKey(pk)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal public key: %w", err)
+	}
+
+	return &pubKey, nil
 }
